c75509ddc81fac4ceef905be6717be6aa0fca5b9
Vincent, Erin <evincent@ariit.com>
2018-10-05 10:53:36 -0600

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Erin Vincent on 10/5/18.
+//  Copyright © 2018 Erin Vincent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
